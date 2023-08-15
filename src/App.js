d25e0d1a541a31dbe910ba399caee20132fc7be1
sushmaakota <118443281+sushmaakota@users.noreply.github.com>
2023-08-15 16:05:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Profile from './components/profile'
+ function App() {
+  return (
+    <div className="container mt-5">
+      <Profile />
+    </div>
+  )
+}
+
+export default App;
